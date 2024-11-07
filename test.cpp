342326b1c148c0255ae7d2316811b6523b873ce7
Kshitij_Dwivedi <kdwivedi@zenon.ai>
2024-11-07 21:06:11 +0530

    added a for loop

--- test.cpp
+++ test.cpp
@@ -6,5 +6,14 @@ int main() {
 int x;
 cin >> x;
 
+vector<int> inp(x, 0);
+for(int i = 0; i < x; i++) {
+    cin >> inp[i];
+}
+
+for(auto val : inp) {
+    cout << val << endl;
+}
+
 cout << x << endl;
 }
